9fca646303f69e6cd79c47782fccd09083967874
cww0128 <1113249273@qq.com>
2022-03-24 18:07:38 +0800

    tagbase

--- route/router.go
+++ route/router.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"github.com/gin-gonic/gin"
+	v1 "thinkgin/app/index/controller/v1"
 	"thinkgin/extend/setting"
 )
 
@@ -12,11 +13,22 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello,router",
-		})
-	})
+	apiv1 := r.Group("/api/v1")
+	{
+		//获取标签列表
+		apiv1.GET("/tags", v1.GetTags)
+		//新增标签
+		apiv1.POST("/tags", v1.AddTag)
+		//编辑标签
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		//删除标签
+		apiv1.DELETE("/tags/:id", v1.DelTag)
+	}
+	//r.GET("/test", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "hello,router",
+	//	})
+	//})
 
 	return r
 }
