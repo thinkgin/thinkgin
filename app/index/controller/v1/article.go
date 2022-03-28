4e563e400b3af248993fc17cc298d51ed6b62b2f
cww0128 <1113249273@qq.com>
2022-03-28 15:11:23 +0800

    articlebase

---
+++ app/index/controller/v1/article.go
@@ -0,0 +1,39 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+/**
+*新增文章
+ */
+func AddArticle(c *gin.Context) {
+	
+}
+
+/**
+*获取文章
+ */
+func GetArticle(c *gin.Context) {
+
+}
+
+/**
+*获取多个文章
+ */
+func GetArticles(c *gin.Context) {
+
+}
+
+/**
+*编辑文章
+ */
+func EditArticle(c *gin.Context) {
+
+}
+
+/**
+* 删除文章
+ */
+
+func DelArticle(c *gin.Context) {
+
+}
--- route/router.go
+++ route/router.go
@@ -24,6 +24,18 @@ func InitRouter() *gin.Engine {
 		apiv1.PUT("/tags/:id", v1.EditTag)
 		//删除标签
 		apiv1.DELETE("/tags/:id", v1.DelTag)
+
+		//——————————————————————————————————————分割线——————————————————————————————————————————
+		//获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		//获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		//新增文章
+		apiv1.POST("/articles", v1.AddArticle)
+		//编辑文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		//删除文章
+		apiv1.DELETE("/articles/:id", v1.DelArticle)
 	}
 	//r.GET("/test", func(c *gin.Context) {
 	//	c.JSON(200, gin.H{
