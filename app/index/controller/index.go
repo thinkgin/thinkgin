8254171689fe7da74418e2a56806fc3e9a5fec57
cww0128 <1113249273@qq.com>
2023-01-02 00:51:11 +0800

    todo

--- app/index/controller/index.go
+++ app/index/controller/index.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Hello Word
-// TODO 日志跟控制台输出改写  - 2023年1月2日00:48:55 -输出的时候输出一段ThinkGin的图画
+// TODO 日志跟控制台输出改写  - 2023年1月2日00:48:55 -输出的时候输出一段ThinkGin的图画-MarkDown这一个版本也要更新一下，然后就是写一个文档
 func HelloWord(c *gin.Context) {
 	name := c.Query("name") /*接收参数*/
 	//maps := make(map[string]interface{})
