97c967f86fbfaed52aaa851688f77b0678854371
cww0128 <1113249273@qq.com>
2023-01-13 14:18:13 +0800

    delruntime

--- app/index/controller/index.go
+++ app/index/controller/index.go
@@ -7,8 +7,7 @@ import (
 	"time"
 )
 
-// Hello Word
-// TODO 日志跟控制台输出改写  - 2023年1月2日00:48:55 -输出的时候输出一段ThinkGin的图画-MarkDown这一个版本也要更新一下，然后就是写一个文档
+// TODO 然后就是写一个文档
 func HelloWord(c *gin.Context) {
 	name := c.Query("name") /*接收参数*/
 	//maps := make(map[string]interface{})
