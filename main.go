fa2e709cfbd239890bb3584909f9935beeb0de1a
cww0128 <1113249273@qq.com>
2023-02-02 00:03:09 +0800

    testbug

--- main.go
+++ main.go
@@ -10,19 +10,19 @@ func main() {
 
 	router := route.InitRouter()
 	s := &http.Server{
-		Addr: fmt.Sprintf(":%d", 8000),
+		Addr: fmt.Sprintf(":%d", 8001),
 		//Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
 		Handler:     router,
-		ReadTimeout: 60,
+		ReadTimeout: 600,
 		//ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout: 60,
+		WriteTimeout: 600,
 		//WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
 
 	log := "\n _________  __        _            __        ______   _             _____       ____    \n|  _   _  |[  |      (_)          [  |  _  .' ___  | (_)           / ___ `.   .'    '.  \n|_/ | | \\_| | |--.   __   _ .--.   | | / ]/ .'   \\_| __   _ .--.  |_/___) |  |  .--.  | \n    | |     | .-. | [  | [ `.-. |  | '' < | |   ____[  | [ `.-. |  .'____.'  | |    | | \n   _| |_    | | | |  | |  | | | |  | |`\\ \\\\ `.___]  || |  | | | | / /_____  _|  `--'  | \n  |_____|  [___]|__][___][___||__][__|  \\_]`._____.'[___][___||__]|_______|(_)'.____.'  \n                                                                                        \n"
 	fmt.Println(log)
-	fmt.Println("控制台输入：localhost:8000 进入首页")
+	fmt.Println("控制台输入：localhost:8001 进入首页")
 
 	s.ListenAndServe()
 }
--- route/router.go
+++ route/router.go
@@ -4,14 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"thinkgin/app/index/controller"
-	"thinkgin/extend/middleware"
 )
 
 /*核心路由*/
 func InitRouter() *gin.Engine {
 	r := gin.New()
-	r.Use(gin.Logger(), gin.Recovery(), middleware.LoggerToFile())
-	gin.SetMode("debug")
+	r.Use(gin.Logger(), gin.Recovery())
+	//r.Use(gin.Logger(), gin.Recovery(), middleware.LoggerToFile())
+	gin.SetMode("release")
+	//gin.SetMode("debug")
 	//gin.SetMode(setting.RunMode)
 	//——————————————————————————————————————分割线-下面是首页数据数据——————————————————————————————————————————
 	index := r.Group("/index/")
